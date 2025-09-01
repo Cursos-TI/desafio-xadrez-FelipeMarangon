b4931c6c00594d445967cf3e3e14f180c4be5026
FelipeMarangon <felipep.marangon@gmail.com>
2025-09-01 22:08:44 +0000

    Modo Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main()  {
+    //VARIÁVEIS
     int a = 0;
     int b = 0;
     int c = 0;
+    int d = 1;
+
     //MOVIMENTAÇÃO DA TORRE
     printf("\nMovimentação da Torre\n");
     while (a < 5) {
@@ -21,4 +24,14 @@ int main()  {
     for (c = 0; c < 8; c++) {
         printf("Esquerda\n");
     }
+
+    //MOVIMENTAÇÃO DO CAVALO
+    printf("\nMovimentação do Cavalo:\n");
+    while (d--){ 
+        for (int i = 0; i < 2; i++) {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+}
+return 0;
 }
